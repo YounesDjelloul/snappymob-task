40bedaae6cb13121aeeacf10e8b4c056282a4cd0
YounesDjelloul <younesdjelloul14@gmail.com>
2025-02-19 19:52:09 +0800

    generator static attributes

---
+++ utils/generator.ts
@@ -0,0 +1,6 @@
+export class Generator {
+    private static readonly CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    private static readonly NUMBERS = '0123456789';
+    private static readonly TARGET_SIZE = 10 * 1024 * 1024;
+
+}
